76ff266c89995b35d84e323b11623dfd9aef3c48
bonavida <gecko.bona@gmail.com>
2021-03-13 21:16:10 +0000

    Deploying to gh-pages from  @ 1ec053a66f397b0264471852e1becee1ac029770 🚀

---
+++ _next/static/jqQduo_eaep-p0U1ihJsz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-b6775306720b40d15616.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-df239edc907522cb16e4.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
