ab7c8f386cb6a7ab874c1740ec2717a544f0d7e2
Diego Bonavida <gecko.bona@gmail.com>
2021-03-13 20:05:24 +0100

    Convert export module back to CommonJS

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
-export const assetPrefix = ghPages ? '/bonavida.github.io/' : '';
+module.exports = {
+  assetPrefix: ghPages ? '/bonavida.github.io/' : '',
+};
