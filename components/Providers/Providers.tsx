51082afd7a59554f8e31c6f694dafc9b8cd887f7
Diego Bonavida <gecko.bona@gmail.com>
2021-03-16 22:16:07 +0100

    Removed unused components

--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import { ThemeProvider } from '@context/themeContext';
-import DarkMode from './DarkMode';
 
 interface ProvidersProps {
   children?: any;
 }
 
 const Providers = ({ children }: ProvidersProps): JSX.Element => (
-  <ThemeProvider>
-    <DarkMode />
-    {children}
-  </ThemeProvider>
+  <ThemeProvider>{children}</ThemeProvider>
 );
 
 export default Providers;
