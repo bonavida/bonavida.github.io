4bd477254f8d8bfb9c2fba1819cbf5e7e3d817c6
Diego Bonavida <gecko.bona@gmail.com>
2021-09-14 01:05:41 +0200

    Add try catch to axios calls

--- src/services/tracks.ts
+++ src/services/tracks.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import axios from 'axios';
 /** Services */
 import { getAllTracksInfo, getTopTracks } from '@services/lastfm';
 /** Utils */
@@ -12,15 +13,15 @@ const TOP_TRACKS_CACHE_PATH = path.join(process.cwd(), '.tracks');
 
 export const fetchTopTracks = async (): Promise<Track[]> => {
   let cachedData;
+  let tracks = [];
 
   // Check if the data is cached
   try {
     cachedData = JSON.parse(
       fs.readFileSync(TOP_TRACKS_CACHE_PATH, 'utf8') ?? null
     );
-  } catch (error) {
-    console.error(error);
-  }
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
 
   // If data is cached and a week has not yet passed, use cached data
   if (
@@ -32,39 +33,42 @@ export const fetchTopTracks = async (): Promise<Track[]> => {
   }
 
   // If not, fetch the data from the Last.fm API and store it in the cache file
-  const { track = [] } = (await getTopTracks()) || {};
+  try {
+    const { track = [] } = (await getTopTracks()) || {};
 
-  const partialTracks: Partial<Track>[] = track.map(({ name, artist }) => ({
-    name,
-    artist: artist.name,
-  }));
-
-  const responses = (await getAllTracksInfo(partialTracks)) || [];
-
-  const tracks = responses
-    .map((res) => res.data)
-    .map(({ track: { mbid, url, name, artist, album } = {} }) => ({
-      mbid: mbid || url,
+    const partialTracks: Partial<Track>[] = track.map(({ name, artist }) => ({
       name,
-      artist: artist?.name || '',
-      album: album?.title || '',
-      image: album?.image
-        ? getLargestImage(album?.image)?.['#text']
-        : '/no-album.png',
+      artist: artist.name,
     }));
 
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
+    const responses = (await getAllTracksInfo(partialTracks)) || [];
+
+    tracks = responses
+      .map((res) => res.data)
+      .map(({ track: { mbid, url, name, artist, album } = {} }) => ({
+        mbid: mbid || url,
+        name,
+        artist: artist?.name || '',
+        album: album?.title || '',
+        image: album?.image
+          ? getLargestImage(album?.image)?.['#text']
+          : '/no-album.png',
+      }));
+
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
 
   try {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
     fs.writeFileSync(
       TOP_TRACKS_CACHE_PATH,
       JSON.stringify({ updatedAt: today.getTime(), data: tracks }),
       { flag: 'w', encoding: 'utf8' }
     );
-  } catch (error) {
-    console.error(error);
-  }
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
 
   return tracks;
 };
