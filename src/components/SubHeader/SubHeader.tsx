dddc1c9b486604e85979fe68f967eeb6089d126a
Diego Bonavida <gecko.bona@gmail.com>
2021-09-16 01:13:44 +0200

    Add back button to posts

--- src/components/SubHeader/SubHeader.tsx
+++ src/components/SubHeader/SubHeader.tsx
@@ -1,12 +1,42 @@
+import cx from 'classnames';
+import { useRouter } from 'next/router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 /** Components */
 import ThemeToggler from '@components/ThemeToggler';
 /** Styles */
 import styles from './SubHeader.module.scss';
 
-const SubHeader = (): JSX.Element => (
-  <div className={styles.subHeader}>
-    <ThemeToggler />
-  </div>
-);
+const SubHeader = (): JSX.Element => {
+  const router = useRouter();
+  const isPost = router.route.includes('/post/');
+
+  const subHeaderClassNames = cx(styles.subHeader, {
+    [styles.subHeaderRight]: !isPost,
+  });
+
+  const handleClick = () => {
+    console.log(router);
+    router.back();
+  };
+
+  return (
+    <div className={subHeaderClassNames}>
+      {isPost && (
+        <button
+          type="button"
+          className={styles.backButton}
+          onClick={handleClick}
+        >
+          <FontAwesomeIcon
+            icon="chevron-left"
+            className={styles.backButtonIcon}
+          />
+          <span>Back</span>
+        </button>
+      )}
+      <ThemeToggler />
+    </div>
+  );
+};
 
 export default SubHeader;
--- src/utils/fontAwesome.ts
+++ src/utils/fontAwesome.ts
@@ -1,11 +1,11 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faGlasses } from '@fortawesome/free-solid-svg-icons';
+import { faGlasses, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 export const registerIcons = (): void => {
   // Add font awesome icons to the library in order to import just used icons
-  library.add(fab, faGlasses, faEnvelope);
+  library.add(fab, faGlasses, faEnvelope, faChevronLeft);
   // Disable add automatically the css styles into the <head> for Next.js
   config.autoAddCss = false;
 };
